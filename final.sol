5dacf1b0aa32a99a677f63e1868322e022c01cf0
EnzoSCYiu <43232452+EnzoSCYiu@users.noreply.github.com>
2020-05-27 17:03:09 +0800

    final version of code

--- final.sol
+++ final.sol
@@ -309,5 +309,20 @@ contract DigitalAssetToken is ERC20_interface, Owned {
        recipient.transfer(revenue);
    }
    
+    function uintToString(uint v) public view returns (string memory str) {
+        uint maxlength = 100;
+        bytes memory reversed = new bytes(maxlength);
+        uint i = 0;
+        while (v != 0) {
+            uint remainder = v % 10;
+            v = v / 10;
+            reversed[i++] = byte(uint8(48 + remainder));
+        }
+        bytes memory s = new bytes(i + 1);
+        for (uint j = 0; j <= i; j++) {
+            s[j] = reversed[i - j];
+        }
+        str = string(s);
+    }
    
 }
